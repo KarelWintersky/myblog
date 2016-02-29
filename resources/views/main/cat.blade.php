23ca46c11f32a2c02f743ae9efb5c003e75e3884
panfillich <tarakan29@gmail.com>
2016-02-29 11:52:43 +0300

    Update cat.blade.php

--- resources/views/main/cat.blade.php
+++ resources/views/main/cat.blade.php
@@ -1,7 +1,7 @@
 <aside>
     <h2>Категории</h2>
     <ul class="nav nav-pills nav-stacked">
-        @foreach($category as $item)
+        @foreach($categories as $item)
           <!-- Еще 1н способ получения URL action('ArticleController@getByCategory',['name'=>$item->name])-->          
           <li {{$_SERVER['REDIRECT_URL']=='/category/'.$item->name?'class=active':''}}>
               <a href="/category/{!!$item->name!!}">{!!$item->name!!}</a>
